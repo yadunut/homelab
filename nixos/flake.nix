ed52a43674d4e7363203c76bbfa4a88372ce0258
Yadunand Prem <yadunand@yadunut.com>
2025-05-21 10:22:50 -0400

    Revert "chore: stick to stable nixos"
    This reverts commit 857fedec9cf9831a60e17b8f41cc497c3bbe5dbb.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
