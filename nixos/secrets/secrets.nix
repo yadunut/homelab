1ad306064acdee79545d6f0dcec9102a2c7d23a7
Yadunand Prem <yadunand@yadunut.com>
2025-11-30 23:44:00 +0800

    remove nixos

--- nixos/secrets/secrets.nix
+++
@@ -1,7 +0,0 @@
-let
-  keys = import ./keys.nix;
-in {
-  "tailscale.age".publicKeys = builtins.attrValues keys;
-  "k3s.age".publicKeys = builtins.attrValues keys;
-  "flux.age".publicKeys = [keys.yadunut keys.yadunut-mbp];
-}
